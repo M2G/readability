9ff27b9cbe71a9a78dbf34bb98be3652e922317d
Matthieu Pierre-louis <m.pierrelouis@hotmail.fr>
2024-06-04 03:24:13 +0200

    feat: add route

---
+++ src/routes/routes.ts
@@ -0,0 +1,34 @@
+import { IncomingMessage, ServerResponse } from 'http';
+import { response } from '../utils';
+import readerController from 'src/controllers';
+import validateUserData from 'src/validation';
+
+/**
+ * Defines the routes and associated controllers for the application.
+ *
+ * @type {Object}
+ * @property {Object} / - The root route.
+ * @property {Object} /users - The users route.
+ * @property {Object} /users/:id - The user by ID route.
+ * @property {Function} notFound - The not found route controller.
+ */
+const routes = {
+  '/': {
+    GET: (request: IncomingMessage, res: ServerResponse) => {
+      response(res, { data: { message: 'running nodejs api' } });
+    },
+  },
+  '/article': {
+    POST: (request: IncomingMessage, res: ServerResponse) => {
+      validateUserData(request, res, readerController);
+    },
+  },
+  notFound: (request: IncomingMessage, res: ServerResponse) => {
+    response(res, {
+      status: 404,
+      data: { message: 'requested resource not found!' },
+    });
+  },
+};
+
+export default routes;
